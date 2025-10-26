b251694db7cf56005a65118ad577c2fff14af186
Hussain22 <hussain.sh.tamimi@gmail.com>
2025-10-26 10:53:28 +0200

    Choose color and random color buttons were added

--- scripts.js
+++ scripts.js
@@ -11,6 +11,7 @@ container.style.border = "1px solid black";
 
 
 
+let squareColor = "blue";
 
 
 function createsquare(){
@@ -23,7 +24,7 @@ square.style.boxSizing = "border-box";
 square.style.border = "1px solid #0f560aff";
 container.appendChild(square);
 square.addEventListener("mousemove", function(){
-    square.style.backgroundColor = "black";
+    square.style.backgroundColor = squareColor;
 })
 
 }
@@ -38,27 +39,33 @@ createsquare();
 
 
 
-let button = document.createElement("button");
-button.textContent = "Reset";
-
-button.style.backgroundColor = "#0f560a";   // لون الخلفية
-button.style.color = "white";               // لون النص
-button.style.fontSize = "18px";             // حجم الخط
-button.style.fontWeight = "bold";           // الخط عريض
-button.style.padding = "10px 25px";         // مسافة داخلية
-button.style.border = "none";               // بدون إطار
-button.style.borderRadius = "8px";          // زوايا دائرية
-button.style.cursor = "pointer";            // شكل اليد عند المرور
-button.style.margin = "15px";               // مسافة حول الزر
-button.style.boxShadow = "0 4px 6px rgba(0,0,0,0.3)"; // ظل خفيف
-button.style.transition = "0.3s";   
+///////////////////////////////////////////////////////////////////////////////
 
 
+let Reset = document.createElement("button");
+Reset.textContent = "Reset";
 
-button.addEventListener("click", function(){
+Reset.style.backgroundColor = "#0f560a";  
+Reset.style.color = "white";               
+Reset.style.fontSize = "18px";             
+Reset.style.fontWeight = "bold";          
+Reset.style.padding = "10px 25px";        
+Reset.style.border = "none";               
+Reset.style.borderRadius = "8px";          
+Reset.style.cursor = "pointer";            
+Reset.style.margin = "15px";               
+Reset.style.boxShadow = "0 4px 6px rgba(0,0,0,0.3)"; 
+Reset.style.transition = "0.3s";   
+
+
+
+
+
+
+Reset.addEventListener("click", function(){
    let input = prompt("Enter number of squares per side:");
 
-    if(!(input >= 1 && input <= 100)){
+    if(!(input >= 1 && input <= 60)){
         alert("Please enter a number between 1 and 100.");
         return;
 
@@ -88,17 +95,12 @@ container.appendChild(square);
 
 
 square.addEventListener("mousemove", function(){
-    square.style.backgroundColor = "black";
+    square.style.backgroundColor = squareColor;
 })
 
 
 
 }
-
-
-
-
-
 
 
 
@@ -109,5 +111,89 @@ square.addEventListener("mousemove", function(){
 
 }
 );
-    
-document.body.prepend(button);
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+let choosColor = document.createElement("button");
+choosColor.textContent = "choosColor";
+
+choosColor.style.backgroundColor = "#0f560a";  
+choosColor.style.color = "white";               
+choosColor.style.fontSize = "18px";             
+choosColor.style.fontWeight = "bold";          
+choosColor.style.padding = "10px 25px";        
+choosColor.style.border = "none";               
+choosColor.style.borderRadius = "8px";          
+choosColor.style.cursor = "pointer";            
+choosColor.style.margin = "15px";               
+choosColor.style.boxShadow = "0 4px 6px rgba(0,0,0,0.3)"; 
+choosColor.style.transition = "0.3s";   
+
+
+choosColor.addEventListener("click", function(){
+
+    colorPicker.click(); 
+
+});
+
+document.querySelector("#colorPicker").addEventListener("input", ()=>{
+    squareColor = document.querySelector("#colorPicker").value;
+});
+  
+
+
+let buttons = document.querySelector(".buttons");
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+ 
+
+
+
+let randomColor = document.createElement("button");
+randomColor.textContent = "randomColor";
+
+randomColor.style.backgroundColor = "#0f560a";  
+randomColor.style.color = "white";               
+randomColor.style.fontSize = "18px";             
+randomColor.style.fontWeight = "bold";          
+randomColor.style.padding = "10px 25px";        
+randomColor.style.border = "none";               
+randomColor.style.borderRadius = "8px";          
+randomColor.style.cursor = "pointer";            
+randomColor.style.margin = "15px";               
+randomColor.style.boxShadow = "0 4px 6px rgba(0,0,0,0.3)"; 
+randomColor.style.transition = "0.3s";   
+
+
+
+
+  function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+
+  
+randomColor.addEventListener("click", function(){
+
+    squareColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+});
+
+
+////////////////////////////////////////////////////////////////////////
+
+buttons.appendChild(Reset);
+buttons.appendChild(choosColor);
+buttons.appendChild(randomColor);
